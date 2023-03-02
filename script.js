25a56508edb8258ddd2c83bc47bed985cf2c602f
andrevolima <126825475+andrevolima@users.noreply.github.com>
2023-03-02 18:41:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO]Verifique os dados e tente novamente')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem' 
+            if (idade >= 0 && idade < 12) { 
+                //Criança
+                img.setAttribute('src', 'bebe-m.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'jovem-m.png')
+            } else if (idade < 60) {
+                //Adulto
+                img.setAttribute('src', 'adulto.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'vovo.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+    
+            if (idade >= 0 && idade < 12) {
+            //Criança)
+            img.setAttribute('src', 'bebe-f.png')
+        } else if (idade < 21) {
+            //Jovem
+            img.setAttribute('src', 'Jovem-f.png')
+        } else if (idade < 60) {
+            //Adulto
+            img.setAttribute('src', 'adulta.png')
+        } else {
+            //Idoso
+            img.setAttribute('src', 'vovo-f.png')
+        }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos: ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+
+}
